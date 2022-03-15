690cbd041852d485bedbc2a048bfef45c2bd3794
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2022-03-15 20:26:40 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React, { useState } from "react";
 import "./styles.css";
 import "./App.css";
 
 const App = () => {
+  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+
   return (
     <div className="App">
       <h1>Stopwatch</h1>

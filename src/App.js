0e0e436aa1ff99e017dce0dd9cba2bbd7621721e
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2022-03-17 16:59:19 +0530

    App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,28 @@ const App = () => {
   const [interv, setInterv] = useState();
   const [status, setStatus] = useState(0);
 
+  var updatedMs = time.ms,
+    updatedS = time.s,
+    updatedM = time.m,
+    updatedH = time.h;
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH++;
+      updatedM = 0;
+    }
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+    if (updatedMs === 60) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
+  };
+
   return (
     <div className="App">
       <Display time={time} />

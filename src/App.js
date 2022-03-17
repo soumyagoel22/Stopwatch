2af78880b5002b1eb82f5f4f3dab72016bee7a9d
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2022-03-17 17:06:49 +0530

    App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ const App = () => {
   const [interv, setInterv] = useState();
   const [status, setStatus] = useState(0);
 
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
   var updatedMs = time.ms,
     updatedS = time.s,
     updatedM = time.m,
@@ -29,6 +35,19 @@ const App = () => {
     updatedMs++;
     return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
   };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ ms: 0, s: 0, m: 0, h: 0 });
+  };
+
+  const resume = () => start();
 
   return (
     <div className="App">

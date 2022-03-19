ae08f6ef68204a59a740af042a4105bdb07fa54f
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2022-03-19 17:14:11 +0530

    App

--- src/Components/BtnDisplay.js
+++ src/Components/BtnDisplay.js
@@ -4,6 +4,22 @@ const BtnDisplay = (props) => {
   return (
     <>
       {props.status === 0 ? <button onClick={props.start}>Start</button> : ""}
+      {props.status === 1 ? (
+        <div>
+          <button onClick={props.stop}>Stop</button>
+          <button onClick={props.reset}>Reset</button>
+        </div>
+      ) : (
+        ""
+      )}
+      {props.status === 2 ? (
+        <div>
+          <button onClick={props.resume}>Resume</button>
+          <button onClick={props.reset}>Reset</button>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 };
